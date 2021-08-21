a50fbaa8c496596d755ed5f43a0b6e2b98c32e50
JeffDing99 <13035070219@163.com>
2021-08-21 10:14:02 +0800

    第一次提交

---
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author layne
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("11111");
+        System.out.println("11111");
+        System.out.println("11111");
+    }
+}
